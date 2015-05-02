05c82b98be85c702360b62d35bc0c6165d24636a
Jefferson <embura@gmail.com>
2015-05-02 10:06:30 -0300

    first commito curso

---
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -0,0 +1,35 @@
+package br.com.caelum.notasfiscais.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import br.com.caelum.notasfiscais.dao.UsuarioDao;
+import br.com.caelum.notasfiscais.modelo.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	private Usuario usuario = new Usuario();
+
+	@Inject
+	UsuarioDao dao = new UsuarioDao();
+
+	public String efetuaLogin() {
+		boolean loginValido = dao.existe(this.usuario);
+		System.out.println("O login era valido ?" + loginValido);
+
+		if (loginValido) {
+			System.out.println("O login era valido ?" + loginValido);
+			return "produto";
+		} else {
+			System.out.println("O login era valido ?" + loginValido);
+			this.usuario = new Usuario();
+			return "login";
+		}
+	}
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+}
---
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -0,0 +1,58 @@
+package br.com.caelum.notasfiscais.mb;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Produto;
+
+@ManagedBean
+public class ProdutoBean {
+
+	private Produto produto = new Produto();
+	private List<Produto> produtos;
+
+	public Produto getProduto() {
+		return this.produto;
+	}
+
+	public void gravar() {
+
+		System.out.println("Será que vai passar aqui ?");
+		ProdutoDao dao = new ProdutoDao();
+
+		if (this.produto.getId() == null) {
+			dao.adiciona(this.produto);
+		} else {
+			dao.atualiza(this.produto);
+		}
+
+		this.produtos = dao.listaTodos();
+		this.produto = new Produto();
+	}
+
+	public List<Produto> getProdutos() {
+		if (produtos == null)
+			produtos = new ProdutoDao().listaTodos();
+		return produtos;
+	}
+
+	public void remove(Produto produto) {
+		ProdutoDao dao = new ProdutoDao();
+		dao.remove(produto);
+		this.produtos = dao.listaTodos();
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Double getValorTotal() {
+		Double total = 0.0;
+		for (Produto produto : this.produtos) {
+			total += produto.getPreco();
+		}
+		return total;
+	}
+}
